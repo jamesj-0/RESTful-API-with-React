59a464ccc172b38274007f9e22b1a0ebf1d11695
CampbellDocherty <campbellsofitsidocherty@gmail.com>
2020-04-22 16:55:52 +0100

    Add code to home route file Relates #17
    Co-authored-by: Ivo-Evans <ievans.storrie@gmail.com>

--- public/routes/home.js
+++ public/routes/home.js
@@ -1,19 +1,64 @@
-import query from '../query.js'
+import query from "../query.js";
 
-const wrapper = document.querySelector('#wrapper');
-const nav = document.querySelector('#navigation');
+const wrapper = document.querySelector("#wrapper");
+const nav = document.querySelector("#navigation");
+
+const loggedIn = `
+<button class="log-out">Log Out</button>
+`;
+
+const loggedOut = `
+<button class="log-in">Log In</button>
+<button class="sign-up">Sign Up</button>
+`;
+
+const allCode = `
+<div id="message"></div>
+<ul></ul>
+`;
 
 function home() {
-   writeToNav()
-   writeToWrapper() 
+    writeToNav();
+    writeToWrapper();
 }
 
-function writeToNav(params) {
-  // check auth token and display accordingly
+function writeToNav({redirect}) {
+    // check auth token and display accordingly
+    const token = localStorage.getItem("token");
+
+    if (!token) {
+        nav.innerHTML = loggedOut;
+    } else {
+        nav.innerHTML = loggedIn;
+        nav.querySelector("#log-out").addEventListener("click", () => {
+            localStorage.removeItem("token");
+            redirect("/");
+        });
+    }
+}
+
+function createListItem(code) {
+    const li = document.createElement("li");
+    const title = document.createElement("h2");
+    title.append(code.title);
+    const language = document.createElement("h3");
+    language.append(code.language);
+    const example = document.createElement("p");
+    example.append(code.example);
+    li.append(title, langauge, example);
 }
 
 function writeToWrapper() {
-    // get db and display anything returned or first 10 things returned
-    wrapper.innerHTML = 
+    wrapper.innerHTML = allCode;
+    query("https://snippetsofcode.herokuapp.com/")
+        .then(json => {
+            const codeSnippets = json.map(code => createListItem(code));
+            wrapper.querySelector("ul").append(...codeSnippets);
+        })
+        .catch(error => {
+            console.error(error);
+            wrapper.querySelector("#message").append("Something went wrong!");
+        });
 }
+
 export default home;
