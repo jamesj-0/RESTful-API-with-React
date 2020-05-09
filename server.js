cb672b5d32a7d36cc392986d1a442a1febe20b73
James-Hardy <jamesjhardy95@gmail.com>
2020-05-09 17:53:04 +0100

    remove server routes

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ const handleError = require("./middleware/error");
 const auth = require("./middleware/auth");
 const examples = require("./handlers/examples-handlers");
 const users = require("./handlers/users-handlers");
-const path = require('path');
+const path = require("path");
 require("dotenv").config();
 
 const PORT = process.env.PORT || 3000;
 const server = express();
 server.use(express.json());
 
-server.get("/all", examples.getAllExamples);
-server.post("/examples", auth, examples.postExample);
-server.get("/examples/:id", examples.getExample);
-server.delete("/examples/:id", auth, examples.deleteExample);
-server.put("/examples/:id", auth, examples.updateExample);
-server.post("/signup", users.signup);
-server.post("/login", users.login);
+// server.get("/all", examples.getAllExamples);
+// server.post("/examples", auth, examples.postExample);
+// server.get("/examples/:id", examples.getExample);
+// server.delete("/examples/:id", auth, examples.deleteExample);
+// server.put("/examples/:id", auth, examples.updateExample);
+// server.post("/signup", users.signup);
+// server.post("/login", users.login);
 
-server.use(express.static('public'));
+server.use(express.static("public"));
 server.use((req, res, next) => {
-    res.status(200).sendFile(path.join(__dirname, '/public/index.html'))
-})
+    res.status(200).sendFile(path.join(__dirname, "/public/index.html"));
+});
 server.use(handleError);
 
 // If this env exists we are in testing mode so don't start the server
